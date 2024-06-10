a18a254cfef810012f46d2aec35b9cb8e8852c3c
alexchidiac <alexchidiac1337@gmail.com>
2024-06-10 10:10:21 +0200

    Fix division by 0 problem

--- waveguide_loss_plotter.py
+++ waveguide_loss_plotter.py
@@ -11,8 +11,8 @@ def plot_waveguide_loss(file_path):
     rcParams['font.size'] = 10
     rcParams['axes.linewidth'] = 1.1
     rcParams['axes.labelpad'] = 10.0
-    plot_color_cycle = cycler('color', ['000000', '0000FE', 'FE0000', '008001', 'FD8000', '8c564b',
-                                        'e377c2', '7f7f7f', 'bcbd22', '17becf'])
+    plot_color_cycle = cycler('color', ['#000000', '#0000FE', '#FE0000', '#008001', '#FD8000', '#8c564b',
+                                        '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'])
     rcParams['axes.prop_cycle'] = plot_color_cycle
     rcParams['axes.xmargin'] = 0
     rcParams['axes.ymargin'] = 0
@@ -36,47 +36,44 @@ def plot_waveguide_loss(file_path):
                      "lines.markerfacecolor" : "None",
                      "lines.markeredgewidth"  : 0.8})
 
-    x2 = []
-    y4 = []
-    y5 = []
-    y6 = []
-    y7 = []
-    y8 = []
-    y9 = []
-
     # Read data from file
     with open(file_path, 'r') as csvfile:
         lines = csv.reader(csvfile, delimiter=',')
-        next(lines)
+        headers = next(lines)
+        num_channels = len(headers) - 1
+        
+        x2 = []
+        y = [[] for _ in range(num_channels)]
+        y_normalized = [[] for _ in range(num_channels)]
+        
         first_value = next(lines)
         for row in lines:
             x2.append(float(row[0]) - float(first_value[0]))
-            y4.append(float(row[1]))
-            y5.append(float(row[2]))
-            y6.append(float(row[3]))
-            y7.append(10 * np.log10(float(first_value[1]) / float(row[1])))
-            y8.append(10 * np.log10(float(first_value[2]) / float(row[2])))
-            y9.append(10 * np.log10(float(first_value[3]) / float(row[3])))
+            for i in range(num_channels):
+                y_value = float(row[i + 1])
+                y[i].append(y_value)
+                if y_value != 0:
+                    y_normalized_value = 10 * np.log10(float(first_value[i + 1]) / y_value)
+                else:
+                    y_normalized_value = 0  # or any appropriate value you want to use in case of zero
+                y_normalized[i].append(y_normalized_value)
 
     # Convert to numpy arrays
-    y7 = np.array(y7)
-    y8 = np.array(y8)
-    y9 = np.array(y9)
     x2 = np.array(x2)
+    y = [np.array(channel) for channel in y]
+    y_normalized = [np.array(channel) for channel in y_normalized]
 
     # Plot the data
     plt.figure(1)
-    plt.plot(x2, y4, label="Right")
-    plt.plot(x2, y5, label="Left")
-    plt.plot(x2, y6, label="Middle")
+    for i in range(num_channels):
+        plt.plot(x2, y[i], label=headers[i + 1])
     plt.ylabel('PD current [A]')
     plt.xlabel('Time [s]')
     plt.legend()
 
     plt.figure(2)
-    plt.plot(x2, y7, label="Right")
-    plt.plot(x2, y8, label="Left")
-    plt.plot(x2, y9, label="Middle")
+    for i in range(num_channels):
+        plt.plot(x2, y_normalized[i], label=headers[i + 1])
     plt.ylabel('Normalized waveguide Loss [dB]')
     plt.xlabel('Time [s]')
     plt.legend()
